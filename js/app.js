b76550448c4e19056c94c9f3cea283ffffdba51b
Farahalkhateeb <farahkhalid96@yahoo.comclear>
2021-06-22 15:35:27 +0300

    new alert

--- js/app.js
+++ js/app.js
@@ -1,57 +1,85 @@
 let userType= prompt('please enter your name')
 console.log('Hi'+ userType);
-let userInfo=prompt('does my favourite food is shawrma?')
+let userInfo=prompt('does my favourite food is shawrma?yes/y no/n')
 switch (userInfo.toLowerCase){
     case"yes":
-    console.log('you got it!')
+    case"y":
+   // console.log('you got it!')
+   alert("you got it!")
     break;
     case"no":
-    console.log('you are near the certin answer')
+    case"n":
+   // console.log('you are near the certin answer')
+   alert("you are near the certin answer ")
     break;
     default:
-        console.log("you didn\'t eat with me any meal")
-}
-let userInfo1=prompt('Do i love cats?')
+        console.log('you didn\'t eat any meal with me')
+let userInfo1=prompt('Do i love cats? yes/y no/n')
 switch(userInfo1.toLowerCase){
     case"yes":
-    console.log('you are right')
+    case"y":
+   // console.log('you are right')
+   alert("you are right")
     break;
     case"no":
-    console.log('you got a mistake')
+    case"n":
+   
+   // console.log('you got a mistake')
+   alert("you got a mistake ")
     break;
     default:
-        console.log('i love cats so much try to git one')
+      //  console.log('i love cats so much try to git one')
+      alert("i love cats so much try to git one")
 
 }
-let userInfo2=prompt('can i cook mansaf?')
+let userInfo2=prompt('can i cook mansaf? yes/y no/n')
 switch(userInfo2.toLowerCase){
     case"yes":
-    console.log('you didn\'t know me')
+    case"y":
+    //console.log('you didn\'t know me')
+    alert("you didn\'t know me")
     break;
     case"no":
-    console.log('come on you know me alot')
+    case"n":
+   
+   // console.log('come on you know me alot')
+   alert("come on you know me alot")
     break;
     default:
-        console.log('i didn\'t know how to cook mansaf yet!')
+       // console.log('i didn\'t know how to cook mansaf yet!')
+       alert("i didn\'t know how to cook mansaf yet!")
 }
 
-let userInfo3=prompt('do i live in Irbid?')
+let userInfo3=prompt('do i live in Irbid? yes/y no/n')
 switch(userInfo3.toLowerCase){
     case"yes":
-    console.log('bravo')
+    case"y":
+   // console.log('bravo')
+   alert("bravo")
     break;
     case"no":
-    console.log('wrong answer')
+    case"n":
+   
+   // console.log('wrong answer')
+   alert("wrong answer")
     break;
     default:
-        console.log('i live in Irbid')
+        //console.log('i live in Irbid')
+        alert("i live in Irbid")
 }
-let userInfo4=prompt('Do I studied art?')
+let userInfo4=prompt('Do I studied art? yes/y no/n')
 switch(userInfo4.toLowerCase){
     case"yes":
-    console.log('oops you are wrong')
+    case"y":
+   // console.log('oops you are wrong')
+   alert("oops you are wrong")
     case"no":
-    console.log('you are right')
+    case"n":
+   
+   // console.log('you are right')
+   alert("you are right")
     default:
-        console.log('i studied communication engineer')
+       // console.log('i studied communication engineer')
+       alert("i studied communication engineer")
+       
 }
